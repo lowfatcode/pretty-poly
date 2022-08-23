5f9aed58ee08def14206e8997d12e5b80da08b32
lowfatcode <jon@magicnumber.co.uk>
2022-08-23 05:42:55 +0100

    refactored rendering to make it supersampling agnostic, changed tile format, better use of buffers when supersampling

--- src/pretty-poly.hpp
+++ src/pretty-poly.hpp
@@ -3,8 +3,6 @@
 
 using namespace std;
 
-#define PP_DEBUG
-
 #ifdef PP_DEBUG
 #define debug(...) printf(__VA_ARGS__)
 #else
@@ -13,28 +11,21 @@ using namespace std;
 
 namespace pretty_poly {
 
-  enum antialias_t {
-    X1      = 0,
-    X2      = 1,
-    X4      = 2
-  };
+  enum antialias_t {X1 = 0, X2 = 1, X4 = 2};
 
   struct rect_t {
-    int x, y;
-    int w, h;    
+    int x, y, w, h;    
 
-    rect_t() :
-      x(0), y(0), w(0), h(0) {}
+    rect_t() : x(0), y(0), w(0), h(0) {}
+    rect_t(int x, int y, int w, int h) : x(x), y(y), w(w), h(h) {}
 
-    rect_t(int x, int y, int w, int h) :
-      x(x), y(y), w(w), h(h) {}
+    bool empty() const {return this->w == 0 && this->h == 0;}
 
-    rect_t intersection(const rect_t &compare) {
+    rect_t intersection(const rect_t &c) {
       return rect_t(
-        max(this->x, compare.x),
-        max(this->y, compare.y),
-        max(0, min(int(this->x + this->w), int(compare.x + compare.w)) - max(this->x, compare.x)),
-        max(0, min(int(this->y + this->h), int(compare.y + compare.h)) - max(this->y, compare.y))
+        max(this->x, c.x), max(this->y, c.y),
+        max(0, min(int(this->x + this->w), int(c.x + c.w)) - max(this->x, c.x)),
+        max(0, min(int(this->y + this->h), int(c.y + c.h)) - max(this->y, c.y))
       );
     }
   };
@@ -42,37 +33,51 @@ namespace pretty_poly {
   struct tile_t {
     rect_t bounds;
     unsigned stride;
-    void *data;
+    uint8_t *data;
 
     tile_t() {};
 
     int get_value(int x, int y) const {
-      uint8_t *p = static_cast<uint8_t*>(this->data) + x + y * this->stride;
-      return (int)*p;
-      // uint32_t *row = (uint32_t *)(&this->data) + ((this->stride / 4) * y);
-      // int dword = x >> 5;
-      // int shift = 31 - (x & 0b11111);
-      // return (row[dword] >> shift) & 0b1;
+      return this->data[x + y * this->stride];
     }
   };
 
-  template<typename T>
-  struct contour_t {
+  template<typename T> struct contour_t {
     T *points;
     unsigned count;
-
     contour_t(T *points, unsigned count) : points(points), count(count) {};
   };
 
   typedef void (*tile_callback_t)(const tile_t &tile);
 
-  tile_callback_t tile_callback;
-  constexpr uint32_t tile_size = 32;
-  rect_t clip(0, 0, 320, 240);
-  antialias_t antialias = X4;
-  
-  void set_options(tile_callback_t callback) {
-    tile_callback = callback;
+  // buffer that each tile is rendered into before callback
+  constexpr unsigned tile_buffer_size = 1024;
+  uint8_t tile_buffer[tile_buffer_size];
+
+  // polygon node buffer handles at most 16 line intersections per scanline
+  // is this enough for cjk/emoji? (requires a 2kB buffer)
+  constexpr unsigned node_buffer_size = 32;
+  int nodes[node_buffer_size][16];
+  unsigned node_counts[node_buffer_size];
+
+  // default tile bounds to X1 antialiasing
+  rect_t tile_bounds(0, 0, tile_buffer_size / node_buffer_size, node_buffer_size);
+
+  // user settings
+  namespace settings {
+    rect_t clip(0, 0, 320, 240);
+    tile_callback_t callback;
+    antialias_t antialias = X1;
+  }  
+
+  void set_options(tile_callback_t callback, antialias_t antialias, rect_t clip) {
+    settings::callback = callback;
+    settings::antialias = antialias;
+    settings::clip = clip;
+
+    // recalculate the tile size for rendering based on antialiasing level
+    int tile_height = node_buffer_size >> antialias;
+    tile_bounds = rect_t(0, 0, tile_buffer_size / tile_height, tile_height);
   }
 
   // dy step (returns 1, 0, or -1 if the supplied value is > 0, == 0, < 0)
@@ -94,10 +99,6 @@ namespace pretty_poly {
     debug("-----------------------\n");
   }
 
-  // polygon node buffer handles at most 32 line intersections per scanline
-  // is this enough for cjk/emoji? (requires a 4kB buffer)
-  static int nodes[tile_size][32];
-  static unsigned node_counts[tile_size];
 
   void add_line_segment_to_nodes(int sx, int sy, int ex, int ey) {
     // swap endpoints if line "pointing up", we do this because we
@@ -107,6 +108,11 @@ namespace pretty_poly {
       swap(sy, ey);
       swap(sx, ex);
     }
+
+    sx <<= settings::antialias;
+    ex <<= settings::antialias;
+    sy <<= settings::antialias;
+    ey <<= settings::antialias;
 
     int x = sx;
     int y = sy;
@@ -125,9 +131,9 @@ namespace pretty_poly {
       // consume accumulated error
       while(e > dy) {e -= dy; x += xinc;}
 
-      if(y >= 0 && y < tile_size) {  
+      if(y >= 0 && y < node_buffer_size) {  
         // clamp node x value to tile bounds
-        int nx = max(min(x, (int)tile_size), 0);        
+        int nx = max(min(x, (int)(tile_bounds.w << settings::antialias)), 0);        
         debug("      + adding node at %d, %d\n", x, y);
         // add node to node list
         nodes[y][node_counts[y]++] = nx;
@@ -147,12 +153,12 @@ namespace pretty_poly {
     int count = contour.count;
 
     // start with the last point to close the loop
-    int lx = (points[(count - 1) * 2 + 0] << antialias) - tile.bounds.x;
-    int ly = (points[(count - 1) * 2 + 1] << antialias) - tile.bounds.y;
+    int lx = points[(count - 1) * 2 + 0] - tile.bounds.x;
+    int ly = points[(count - 1) * 2 + 1] - tile.bounds.y;
 
     while(count--) {
-      int x = ((*points++) << antialias) - tile.bounds.x;
-      int y = ((*points++) << antialias) - tile.bounds.y;
+      int x = (*points++) - tile.bounds.x;
+      int y = (*points++) - tile.bounds.y;
 
       add_line_segment_to_nodes(lx, ly, x, y);
       
@@ -160,11 +166,8 @@ namespace pretty_poly {
     };
   }
 
-  void render_tile(const tile_t &tile) {
-    // clear the tile data
-    memset(tile.data, 0, tile.stride * tile.bounds.h);
-
-    for(auto y = 0; y < tile.bounds.h; y++) {
+  void render_nodes(const tile_t &tile) {
+    for(auto y = 0; y < node_buffer_size; y++) {
       if(node_counts[y] == 0) {
         continue;
       }
@@ -181,10 +184,8 @@ namespace pretty_poly {
 
         debug(" - render span at %d from %d to %d\n", y, sx, ex);
 
-        uint8_t *p = static_cast<uint8_t*>(tile.data) + sx + y * tile.stride;
-        int count = ex - sx;
-        while(count--) {
-          *p++ = 1;
+        for(int x = sx; x < ex; x++) {
+          tile.data[(x >> settings::antialias) + (y >> settings::antialias) * tile.stride]++;
         }       
       }
     }
@@ -200,7 +201,6 @@ namespace pretty_poly {
   
   template<typename T>
   void draw_polygon(std::vector<contour_t<T>> contours) {    
-    static uint8_t tile_buffer[tile_size][tile_size];
 
     debug("> draw polygon with %lu contours\n", contours.size());
 
@@ -209,7 +209,7 @@ namespace pretty_poly {
     T miny = contours[0].points[1], maxy = miny;
   
     // TODO: apply arbitrary transform of points if requested
-    // question:  is this a feature we want to add?
+    // question:  is this a feature we want to add to pretty poly?
 
     // find min/max bounds of polygon
     for(auto &contour : contours) {
@@ -221,34 +221,30 @@ namespace pretty_poly {
       }
     }
 
-    // scale bounds for supersampling
-    minx <<= antialias; maxx <<= antialias;
-    miny <<= antialias; maxy <<= antialias;
-
     debug("  - bounds %d, %d -> %d, %d\n", minx, miny, maxx, maxy);
-    debug("  - clip %d, %d (%d x %d)\n", clip.x, clip.y, clip.w, clip.h);
+    debug("  - clip %d, %d (%d x %d)\n", settings::clip.x, settings::clip.y, settings::clip.w, settings::clip.h);
 
     // iterate over tiles
     debug("  - processing tiles\n");
-    for(auto y = miny; y < maxy; y += tile_size) {
-      for(auto x = minx; x < maxx; x += tile_size) {
+    for(auto y = miny; y < maxy; y += tile_bounds.h) {
+      for(auto x = minx; x < maxx; x += tile_bounds.w) {
         tile_t tile;
-        // get intersection of tile with clip rect
-        tile.bounds = rect_t(x, y, tile_size, tile_size).intersection(rect_t(clip.x << antialias, clip.y << antialias, clip.w << antialias, clip.h << antialias));
+        tile.bounds = rect_t(x, y, tile_bounds.w, tile_bounds.h).intersection(settings::clip);
+        tile.stride = tile_bounds.w;
+        tile.data = tile_buffer;
         debug("    : %d, %d (%d x %d)\n", tile.bounds.x, tile.bounds.y, tile.bounds.w, tile.bounds.h);
+
         // if no intersection then skip tile
-        if(tile.bounds.w == 0 || tile.bounds.h == 0) {
+        if(tile.bounds.empty()) {
           debug("    : empty when clipped, skipping\n");
           continue;
         }
 
-        // set stride of tile in bytes
-        tile.stride = tile_size;
-        tile.data = (void *)tile_buffer;
+        // clear existing tile data and nodes
+        memset(node_counts, 0, sizeof(node_counts));
+        memset(tile.data, 0, tile_buffer_size);
 
         // build the nodes for each contour
-        memset(node_counts, 0, sizeof(node_counts));
-
         for(auto &contour : contours) {
           debug("    : build nodes for contour\n");
           build_nodes(contour, tile);
@@ -256,9 +252,9 @@ namespace pretty_poly {
 
         debug("    : render the tile\n");
         // render the tile
-        render_tile(tile);
+        render_nodes(tile);
 
-        tile_callback(tile);
+        settings::callback(tile);
       }
     }
   }
